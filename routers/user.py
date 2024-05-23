edab51f4bbc3975ed938e266ccc965bf82086d4a
KirtVik <55051710+KirtVik@users.noreply.github.com>
2024-05-23 10:10:20 +0300

    Update user.py

--- routers/user.py
+++ routers/user.py
@@ -2,17 +2,29 @@ from flask import render_template, Blueprint, request
 
 mod = Blueprint('mod', __name__)
 
-
 @mod.route('/')
 def index():
     return render_template('index.html')
 
 
-@mod.route('/auth')
+@mod.route('/auth', methods=['GET','POST'] )
 def auth():
+    if request.method =='POST':
+        print(request.form.to_dict())   
     return render_template('auth.html')
 
 
+@mod.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+@mod.route('/catalog')
+def cat():
+    return render_template('catalog.html')
+
+@mod.route('/trash')
+def trash():
+    return render_template('trash.html')
 
 @mod.route('/profile', methods=['GET', 'POST'])
 def profile():
